d6c0f2f6505626870aa47006d5779990b3a59939
CherryCold <601799041@qq.com>
2019-01-22 17:59:49 +0800

    了解关于http服务器启动的两个函数的具体内容

--- README.md
+++ README.md
@@ -4,3 +4,15 @@
 
 首先，最简单的使用就是http.HandleFunc和http.ListenAndServe
 HandleFunc用来处理客户端发来的http请求
+第一个参数用来指定路由的路径
+ListenAndserve用来监听请求，然后创建一个goroutine去进行服务
+第二个参数可以自定义路由的内容
+
+（才知道go的方法是有重写这一说的，学习了）
+
+其中Request负责接收请求的内容
+Method是HTTP方法	GET POST等等
+ResponseWriter负责写入返回的内容
+返回头WriterHeader
+返回内容Write
+
--- src/httpstudy/http.go
+++ src/httpstudy/http.go
@@ -1,14 +1,48 @@
 package httpstudy
 
-import "net/http"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
-func HandlerIndex(w http.ResponseWriter, r *http.Request) {
+//构造http响应，   http接收到的请求
+func HandleLogin(w http.ResponseWriter, r *http.Request) {
 	/* if r.Method != "POST" {
 		return
 	} */
+	//Method是请求方法
+	if r.Method == "POST" {
+		data, err := ioutil.ReadAll(r.Body)
+		fmt.Println(data)
+		if err != nil {
+			w.WriteHeader(400)
+			fmt.Println("400")
+			w.Write([]byte(err.Error()))
+			return
+		}
+		w.WriteHeader(200)
+		//将返回的内容保存在w中
+		fmt.Fprintln(w, "200OK")
+		fmt.Println("200")
+		w.Write(data)
+		return
+	}
 	w.Write([]byte("hello,world"))
 }
+
+type handelers func(http.ResponseWriter, *http.Request)
+type Handlers map[string]handelers
+
 func Start() {
-	http.HandleFunc("/", HandlerIndex)
+	//登陆就注册
+	handlers := Handlers{
+		"/login": HandleLogin,
+	}
+	for pattern, hander := range handlers {
+		http.HandleFunc(pattern, hander)
+	}
+	//第二个参数是自定义路由方法
+	//通过重写ServeHTTP方法来实现
 	http.ListenAndServe(":8080", nil)
 }
